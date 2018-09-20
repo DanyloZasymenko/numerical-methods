89e9846d51d87a108f840ddd8c575b09becf2ac8
unknown <danik1492@gmail.com>
2018-09-20 09:04:34 +0300

    still does not work

--- src/main/java/Lab_1.java
+++ src/main/java/Lab_1.java
@@ -1,5 +1,4 @@
 import implementations.GaussMethod;
-import interfaces.Method;
 
 import java.io.FileNotFoundException;
 
@@ -11,15 +10,18 @@ public class Lab_1 {
     public static void main(String[] args) throws FileNotFoundException {
 
         double[][] matrix = readMatrixFromFile("matrix.txt");
+//        double[][] matrix = readMatrixFromFile("matrix1.txt");
+//        double[][] matrix = readMatrixFromFile("matrix2.txt");
+//        double[][] matrix = readMatrixFromFile("matrix3.txt");
 
-        Method method = new GaussMethod();
+        GaussMethod method = new GaussMethod();
         System.err.println("Input matrix: ");
         printMatrix(matrix);
         if (matrix.length == matrix[0].length) {
             System.err.println("Inverse matrix: ");
             printMatrix(method.invert(matrix));
-            System.err.println("Result of multiplication: ");
-            printMatrix(multiplyMatrix(matrix, method.invert(matrix)));
+//            System.err.println("Result of multiplication: ");
+//            printMatrix(multiplyMatrix(matrix, method.invert(matrix)));
         } else if (matrix[0].length - matrix.length == 1) {
             double[][] a = getWithoutLastColumn(matrix);
             double[] b = getLastColumn(matrix);
